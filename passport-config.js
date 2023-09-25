20527493654d58f33019e6ceb5457469221f0232
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-09-25 13:28:15 +0100

    Configure register page and hashedPassword method for hashing

--- passport-config.js
+++ passport-config.js
@@ -1,24 +1,26 @@
 const localStrategy = require("passport-local").Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 function initialize(passport, getUserByEmail) {
   const authenticateUser = (email, password, done) => {
     const user = getUserByEmail(email);
-    if(user == null){
-        return done(null, false , {message : 'No user with that email !'})
+    if (user == null) {
+      return done(null, false, { message: "No user with that email !" });
     }
 
     try {
-        if(bcrypt.compare(password,user.password)){
-            return done(null, user);
-        } else{
-            return done(null, false, {message : 'Password incorrect'})
-        }
+      if (bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Password incorrect" });
+      }
     } catch (e) {
-        return(e);
+      return e;
     }
   };
   passport.use(new localStrategy({ usernameField: "email" }), authenticateUser);
   passport.serializeUser((user, done) => {});
   passport.deserializeUser((id, done) => {});
 }
+
+module.exports = initialize;
--- server.js
+++ server.js
@@ -1,46 +1,62 @@
-const express = require('express');
-const app = express();
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const initializePassport = require('./passport-config');
-initializePassport(passport);
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
+const initializePassport = require("./passport-config");
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email)
+);
 
 const users = [];
 
-app.set('view-engine','ejs');
+app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) =>{
-    res.render('index.ejs',{name : "Hello World!"});
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "Hello World!" });
+});
 
-app.get("/login", (req, res) =>{
-    res.render('login.ejs');
-})
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
-app.post("/login",(req,res)=>{
+app.post("/login", (req, res) => {});
 
-})
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
 
-app.get("/register", (req, res) =>{
-    res.render('register.ejs');
-})
-
-app.post("/register",(req,res)=>{
-    try {
-        const hashedPassword = bcrypt.hash(req.body.password, 10);
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login');
-    } catch {
-        res.redirect('/register');
-    }
-    console.log(users);
-})
+app.post("/register", (req, res) => {
+  try {
+    const hashedPassword = bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+  console.log(users);
+});
 
 app.listen(3000);
