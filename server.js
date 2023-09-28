6e2d02afe3c39f255e58e8cc472a58b3e45e0d3d
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-09-28 01:25:25 +0100

    Fix flash method syntax

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.use(flash);
+app.use(flash());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
