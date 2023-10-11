0be0aafb7c301c65b9286c16f8bea253f5bfaa28
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-10-11 18:55:19 +0100

    Add condition in password least 6 characters and show error message in register page

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
-const methodOverride = require('method-override');
-const { body, validationResult } = require('express-validator');
+const methodOverride = require("method-override");
+const { body, validationResult, check } = require("express-validator");
 
 const initializePassport = require("./passport-config");
 initializePassport(
   passport,
-  email => users.find((user) => user.email === email),
-  id => users.find((user) => user.id === id)
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 const users = [];
@@ -32,7 +32,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 app.get("/", checkAuthenticated, (req, res) => {
   res.render("index.ejs", { name: req.user.name });
@@ -42,51 +42,69 @@ app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", passport.authenticate('local',{
+app.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
     failureFlash: true
-}));
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    });
-    res.redirect("/login");
-  } catch {
-    res.redirect("/register");
-  }
-  console.log(users);
-});
+app.post(
+  "/register",
+  [
+    check("name").notEmpty().withMessage("Name is required"),
+    check("email").isEmail().withMessage("Invalid email address"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters")
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("register.ejs", { errors: errors.array() });
+    }
 
-app.delete('/logout', (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      });
+      res.redirect("/login");
+    } catch {
+      res.redirect("/register");
+    }
+    console.log(users);
+  }
+);
+
+app.delete("/logout", (req, res) => {
   req.logOut(() => {
-    res.redirect('/login');
+    res.redirect("/login");
   });
 });
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return next()
+    return next();
   }
 
-  res.redirect('/login')
+  res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  next()
+  next();
 }
 
 app.listen(3000);
